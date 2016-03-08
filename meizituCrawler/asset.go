7643389fe0195c7588214ee9c80f6b21da52ad3a
pein0119 <pein0119@gmail.com>
2016-03-08 17:34:18 +0800

    重构数据结构

--- meizituCrawler/asset.go
+++ meizituCrawler/asset.go
@@ -1,5 +1,6 @@
 package meizituCrawler
 
+// 标签和专辑
 type Asset struct {
 	name       string // 名称
 	url        string // 链接
@@ -12,4 +13,19 @@ func NewAsset(name string, url string) *Asset {
 	asset.url = url
 	
 	return asset
+}
+
+type Image struct {
+	Asset
+	albumName string			// 图片所属专辑
+}
+
+func NewImage(name string, url string, albumName string) *Image {
+	image := new(Image)
+
+	image.name = name
+	image.url = url
+	image.albumName = albumName
+
+	return image
 }
--- meizituCrawler/crawler.go
+++ meizituCrawler/crawler.go
@@ -3,27 +3,31 @@ package meizituCrawler
 import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/parnurzeal/gorequest"
+	"github.com/qiniu/iconv"
 	"io/ioutil"
-	"os"
+	"path/filepath"
+	"sync"
+	"fmt"
 )
 
 type Crawler struct {
-	saveDir string
-	tagQueue   chan Tag
-	albumQueue chan Album
-	imageQueue chan Image
-	stop      chan int
-	wg        *sync.WaitGroup
+	saveDir    string
+	tagQueue   chan *Asset
+	albumQueue chan *Asset
+	imageQueue chan *Image
+	stop       chan int
+	wg         *sync.WaitGroup
 }
 
 func NewCrawler(saveDir string) *Crawler {
 	c := new(Crawler)
 
 	c.saveDir = saveDir
-	c.tagChan = make(chan Tag)
-	c.imageChan = make(chan Image)
+	c.tagQueue = make(chan *Asset)
+	c.albumQueue = make(chan *Asset)
+	c.imageQueue = make(chan *Image)
 	c.stop = make(chan int)
-	
+
 	return c
 }
 
@@ -35,7 +39,7 @@ func (c *Crawler) run() error {
 		close(c.stop)
 	}()
 
-	startUrl = "http://www.meizitu.com/"
+	startUrl := "http://www.meizitu.com/"
 	doc, err := goquery.NewDocument(startUrl)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -43,7 +47,6 @@ func (c *Crawler) run() error {
 
 	doc.Find(".tags a").Each(func(_ int, s *goquery.Selection) {
 		url, _ := s.Attr("href")
-
 		title := s.Text()
 		cd, err := iconv.Open("utf-8", "gb2312")
 		if err != nil {
@@ -52,44 +55,44 @@ func (c *Crawler) run() error {
 		defer cd.Close()
 		title = cd.ConvString(title)
 
-		tag := NewTag(title, url)
+		tag := NewAsset(title, url)
 		c.tagQueue <- tag
 	})
 
 	for {
 		select {
-		case tag := <- tagQueue:
+		case tag := <-c.tagQueue:
 			go c.downloadTag(tag)
-		case album := <- albumQueue:
+		case album := <-c.albumQueue:
 			go c.downloadAlbum(album)
-		case image := <- imageQueue:
+		case image := <-c.imageQueue:
 			go c.downloadImage(image)
 		case <-c.stop:
-			return
+			return nil
 		}
 	}
 }
 
-func (c *Crawler) downloadTag(Tag) error {
+func (c *Crawler) downloadTag(tag *Asset) error {
 	
+	return nil
 }
 
-func (c *Crawler) downloadAlbum(album) error {
-	
+func (c *Crawler) downloadAlbum(album *Asset) error {
+	return nil
 }
 
-
-func (c *Crawler) downloadImage(image) error {
+func (c *Crawler) downloadImage(image *Image) error {
 	request := gorequest.New()
 	_, body, err := request.Get(image.url).End()
 
 	if err != nil {
-		return 1
-	}
-
-	if FileExist(path) {
 		return nil
 	}
-	fileName := c.saveDir + os.PathSeparator +  image.name
+
+	fileName := filepath.Join(c.saveDir, image.albumName, image.name)
+	
 	ioutil.WriteFile(fileName, []byte(body), 0644)
+
+	return nil
 }
